96b55e36401774df50d3ff91191d70641f4195d4
BigTicket5 <dkrose.random@gmail.com>
2014-05-30 14:34:40 +0800

    V2.2

--- app/config.py
+++ app/config.py
@@ -11,6 +11,17 @@ SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
         { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
         { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-        { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+        { 'name': 'AOSSSL', 'url': 'http://openid.aol.com/<username>' },
         { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-        { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+        { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' },
+        { 'name': 'Baidu','url':'http://www.i.baidu.com'  }
+        ]
+
+#mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+#administrator list
+ADMINS=['dkrose.random@gmail.com']
